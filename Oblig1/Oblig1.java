91e364cd71e6087c920c989698fb80ead9ac735a
jakobskr <jakobskr@ifi.uio.no>
2019-02-01 11:44:05 +0100

    Done with the programming part of the assignment

--- Oblig1/Oblig1.java
+++ Oblig1/Oblig1.java
@@ -5,38 +5,56 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 public class Oblig1 {
-	int n = 2000000;
+	int n = 0;
 	int[] a = new int[n];
 	int[] b = new int[n];
-	int k = 100;
+	int k = 20;
 	CyclicBarrier cb;
 
 
-
 	public static void main(String[] args) {
+		Oblig1 task = new Oblig1();
+		task.k = 20;
+		for (int i = 1000; i < 1000000000 ; i= i * 10) {
+			task.does(i,20);
+
+		}
+		System.out.println(" ");
+
+		for (int i = 1000; i < 1000000000 ; i= i * 10) {
+			task.does(i,100);
+		}
+	}
+
+	public void does(int nx, int kx) {
+		n = nx;
+		k = kx;
+		Double[] seq = new Double[7];
+		Double[] para = new Double[7];
+
 		Random random = new Random();
 		for (int j = 0; j < 7; j++) {
-			Oblig1 t = new Oblig1();
-			for (int i = 0;i < t.n ;i++ ) {
-				int d = random.nextInt(t.n);
-				t.a[i] = d;
-				t.b[i] = d;
+		a = new int[n];
+		b = new int[n];
+
+			for (int i = 0;i < n ;i++ ) {
+				int d = random.nextInt(n);
+				a[i] = d;
+				b[i] = d;
 			}
 
-		
-		//System.out.println("before: " + Arrays.toString(t.b));
-
 		Long t2 = System.nanoTime();
-		t.task2();
-		Double time2 = (System.nanoTime()-t2)/1000000.0;
+		task2();
+		para[j] = (System.nanoTime()-t2)/1000000.0;
 
 		Long t1 = System.nanoTime();
-		t.task1();
-		Double time1 = (System.nanoTime()-t1)/1000000.0;
+		task1();
+		seq[j] = (System.nanoTime()-t1)/1000000.0;
 		
-		
-		System.out.printf("Seq: %f Para:%f S: %f\n", time1, time2, time1/time2);
 		}
+		Arrays.sort(seq);
+		Arrays.sort(para);
+		System.out.printf("N: %d k: %d Seq: %f Para:%f S: %f\n", n, k, seq[3], para[3], seq[3]/para[4]);
 
 	}
 
@@ -44,7 +62,7 @@ public class Oblig1 {
 	public void task1() {
 		
 		insertSort(b, k);
-
+		//System.out.println("before: " + Arrays.toString(b));
 
 		int tmp;
 		for (int j = k; j < b.length; j++) {
@@ -52,7 +70,7 @@ public class Oblig1 {
 				tmp = b[j];
 				b[j] = b[k - 1];
 				b[k - 1] = tmp;
-				insertSort(b, k);
+				insertSort(b, k, 0);
 			}
 		}
 
@@ -60,7 +78,7 @@ public class Oblig1 {
 	}
 
 	public void task2() {
-		int threads = 3;
+		int threads = 4;
 		Worker[] workers = new Worker[threads];
 		int workSegment = a.length - k;
 		int segmentLenght = workSegment / threads;
@@ -71,16 +89,17 @@ public class Oblig1 {
 
  		cb = new CyclicBarrier(threads + 1);
 
-
 		for (int i = 0; i < threads - 1;i ++ ) {
 			workers[i] = new Worker(start, end);
 			start = end;
 			end = end + segmentLenght;
-			new Thread(workers[i]).start();
 		}
 
 		workers[threads - 1] = new Worker(start, end + workSegment % threads);
-		new Thread(workers[threads - 1]).start(); 
+		for (int i = 0; i < threads ;i ++ ) {
+			new Thread(workers[i]).start(); 
+		}
+
 		insertSort(a, k);
 
 		try {
@@ -89,18 +108,17 @@ public class Oblig1 {
 		catch(Exception e){return;}
 
 		for (Worker w: workers) {
-			for (int j = w.start;j < w.start + k; j++ ) {
+			for (int j = w.start;j < w.end; j++ ) {
 				if (a[j] > a[k - 1]) {
 					tmp = a[j];
 					a[j] = a[k - 1];
 					a[k - 1] = tmp;
-					insertSort(a,k);
+					insertSort(a,k,0);
 				}
 
 				else break;
 			}
 		}
-		//System.out.println(Arrays.toString(a));
 
 	}
 
@@ -118,14 +136,16 @@ public class Oblig1 {
 	}
 
 	public void insertSort(int a[], int k, int start) {
-		for (int j = start; j < start + k - 1; j++ ) {
-			int i = j;
-			int tmp;
-			while(i >= 0 && a[i] < a[i + 1]) {
-				tmp = a[i];
-				a[i] = a[i + 1];
-				a[i + 1] = tmp;
-				i--; 
+		//TODO: Find out why this is so slow; 
+		int tmp;
+		for (int j = start + k - 1; j > start; j-- ) {
+			if (a[j] < a[j - 1]) {
+				return;
+			}
+			else {
+				tmp = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = tmp;
 			}
 		}
 	}
@@ -146,9 +166,9 @@ public class Oblig1 {
 		public void run() {
 		int tmp;
 		int ind = start + k - 1;
-		for (int j = start; j < start + k - 1; j++ ) {
+		for (int j = start; j < ind; j++ ) {
 			int i = j;
-			while(i >= 0 && a[i] < a[i + 1]) {
+			while(i >= start && a[i] < a[i + 1]) {
 				tmp = a[i];
 				a[i] = a[i + 1];
 				a[i + 1] = tmp;
@@ -156,13 +176,23 @@ public class Oblig1 {
 			}
 		}
 
-		for (int j = ind; j < end - 1; j++) {
+		for (int j = ind + 1; j < end; j++) {
 			if (a[j] > a[ind]) {
 				tmp = a[j];
 				a[j] = a[ind];
 				a[ind] = tmp;
 				
-				insertSort(a, k, start);
+				//insertSort(a, k, start);
+				for (int l = start + k - 1; l > start; l-- ) {
+					if (a[l] < a[l - 1]) {
+						break;
+					}
+					else {
+						tmp = a[l];
+						a[l] = a[l - 1];
+						a[l - 1] = tmp;
+					}
+				}
 			}
 			//System.out.println(start + "  " + j);
 		}
